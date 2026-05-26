a00534435e861b952ea7df817cab7c9a94cfcbc6
guruvujk <guruvu@gmail.com>
2026-05-26 12:53:31 +0530

    Add auto browser open to start.ps1

--- start.ps1
+++ start.ps1
@@ -1,2 +1,3 @@
 Start-Process powershell -ArgumentList @("-NoExit", "-Command", "cd C:\Users\JKRAOWIN\mailblast\backend; node server.js")
 Start-Process powershell -ArgumentList @("-NoExit", "-Command", "`$env:PORT=3000; cd C:\Users\JKRAOWIN\mailblast\frontend; npm start")
+Start-Process "https://mailblast-fyz1.onrender.com"
