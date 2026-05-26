ef7b6e037117441886ce0428772cb7c4868f1b60
guruvujk <guruvu@gmail.com>
2026-05-26 10:08:18 +0530

    Fix redirect URI to use env var

--- backend/services/gmail.js
+++ backend/services/gmail.js
@@ -5,7 +5,7 @@ function createOAuthClient() {
   return new google.auth.OAuth2(
     process.env.GOOGLE_CLIENT_ID,
     process.env.GOOGLE_CLIENT_SECRET,
-    'http://localhost:3001/auth/google/callback'
+    process.env.GOOGLE_REDIRECT_URI
   );
 }
 
